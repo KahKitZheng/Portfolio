4528deba752463ca186977ad7af3ac990955ddaa
Kah Kit Zheng <kahkitzheng@gmail.com>
2020-04-23 15:08:38 +0200

    Init project

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <header>
+      <nav className="navigation">
+        <div className="navigation__list">
+          <p className="navigation__list__item"></p>
+          <p className="navigation__list__item"></p>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navigation;
