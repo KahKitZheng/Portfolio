b48d85ea27864241909d152e7668d835bd34017d
Kah Kit Zheng <kahkitzheng@gmail.com>
2020-04-25 17:26:39 +0200

    Feature: Project cards are generated using json data

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,24 +1,29 @@
 import React from "react";
-import folderIcon from "../folder.svg";
+import GithubIcon from "./githubIcon";
 
-const card = () => {
+const card = (props) => {
+  const { title, description, link, tags } = props.project;
+
   return (
     <div className="card">
-      <img src={folderIcon} className="card__icon" alt="" />
-      <h3 className="card__title">Project title</h3>
+      <div className="card__icons">
+        <h3 className="card__title">{title ? title : "T.B.D."}</h3>
+        {link ? (
+          <a href={link} target="_blank" rel="noopener noreferrer">
+            <GithubIcon />
+          </a>
+        ) : null}
+      </div>
       <p className="card__description">
-        Lacus, cras condimentum ornare volutpat ultricies. Mollis erat faucibus
-        facilisis gravida nibh sit sapien est. Proin integer commodo sit
-        malesuada aenean pellentesque ante aenean posuere.
+        {description ? description : "Probably something cool!"}
       </p>
 
       <div className="card__tags">
-        <p className="card__tag">React</p>
-        <p className="card__tag">Vue</p>
-        <p className="card__tag">Angular</p>
-        <p className="card__tag">Kotlin</p>
-        <p className="card__tag">Java</p>
-        <p className="card__tag">React-Native</p>
+        {tags.length > 0 ? (
+          tags.map((tag) => <p className="card__tag">{tag}</p>)
+        ) : (
+          <p className="card__tag">😎</p>
+        )}
       </div>
     </div>
   );
--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import Segment from "./segment";
 import Card from "./card";
+import list from "../constants/projects.json";
 
 const projects = () => {
   return (
     <section id="projects">
       <Segment name={"Projects"}>
         <div className="card__list container">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {list.map((project) => (
+            <Card project={project} />
+          ))}
         </div>
       </Segment>
     </section>
